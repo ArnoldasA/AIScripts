43aa9c52ad323849565ced6ba90a613edfdbdebe
ArnoldasA <74542476+ArnoldasA@users.noreply.github.com>
2020-11-29 14:03:33 +0000

    Update State.cs

--- Scripts/State.cs
+++ Scripts/State.cs
@@ -5,27 +5,30 @@ using UnityEngine.AI;
 
 public class State : MonoBehaviour
 {
+//AI states
     public enum STATE
     {
         IDLE,PATROL,PURSUIT,ATTACK,SLEEP
     };
+    //Switching between states
     public enum EVENT
     {
         ENTER,UPDATE,EXIT
     };
-
-    public STATE name;//enum
+ //declaring variables
+    public STATE name;
     protected EVENT stage;
     protected GameObject npc;
     protected Animator anim;
     protected Transform player;
-    protected State nextState;//not enum
+    protected State nextState;
     protected NavMeshAgent agent;
-
+   
     float visDist = 10f;
     float visAngle = 30f;
     float shootdis = 7f;
 
+//Setting our AI, navmesh , animator , animations and player transform 
     public State(GameObject _npc,NavMeshAgent _agent,Animator _anim,Transform _player)
     {
         npc = _npc;
@@ -35,7 +38,7 @@ public class State : MonoBehaviour
         player = _player;
 
     }
-
+//These switches our states
     public virtual void Enter() { stage = EVENT.UPDATE; }
     public virtual void Update() { stage = EVENT.UPDATE; }
     public virtual void Exit() { stage = EVENT.EXIT; }
@@ -52,7 +55,7 @@ public class State : MonoBehaviour
     }
 }
 
-
+//States of the AI and what it does during these states
 public class Idle: State
 {
     public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
@@ -80,7 +83,7 @@ public class Idle: State
         base.Exit();
     }
 }
-
+//Ai has set waypoints and goes across them unless player interaction breaks this state
 public class Patrol : State
 {
     int currentIndex = -1;
